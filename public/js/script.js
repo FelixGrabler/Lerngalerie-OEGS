636c38f32acae8ce22ca4c88ac7494787fbabab3
Felix Grabler <grabler@student.tugraz.at>
2023-12-19 19:26:33 +0100

    added nav bar with control options for blur and shuffle

--- public/js/script.js
+++ public/js/script.js
@@ -66,3 +66,27 @@ function initIntersectionObserver() {
     observer.observe(video);
   });
 }
+
+// blur
+function toggleVisibility(option) {
+  const videos = document.querySelectorAll("video");
+  const titles = document.querySelectorAll(".video-title");
+
+  videos.forEach((video) => {
+    video.classList.remove("video-blur");
+  });
+
+  titles.forEach((title) => {
+    title.classList.remove("title-blur");
+  });
+
+  if (option === "videos") {
+    videos.forEach((video) => {
+      video.classList.add("video-blur");
+    });
+  } else if (option === "titles") {
+    titles.forEach((title) => {
+      title.classList.add("title-blur");
+    });
+  }
+}
