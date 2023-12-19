8e360cc7cbb1cb445e80e9c5ae7658c0a47208ef
Felix Grabler <grabler@student.tugraz.at>
2023-12-19 19:46:17 +0100

    added shuffle functionality

--- public/js/script.js
+++ public/js/script.js
@@ -68,7 +68,7 @@ function initIntersectionObserver() {
 }
 
 // blur
-function toggleVisibility(option) {
+function toggleVisibility(option, event) {
   const videos = document.querySelectorAll("video");
   const titles = document.querySelectorAll(".video-title");
 
@@ -88,5 +88,58 @@ function toggleVisibility(option) {
     titles.forEach((title) => {
       title.classList.add("title-blur");
     });
+  }
+
+  // Update button appearance
+  const buttons = document.querySelectorAll(".blur-btn");
+  buttons.forEach((btn) => btn.classList.remove("selected"));
+  event.currentTarget.classList.add("selected");
+}
+
+// shuffle
+let originalState = {};
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Save the original state of each category container
+  document
+    .querySelectorAll(".category-container")
+    .forEach((container, index) => {
+      originalState[index] = container.innerHTML;
+    });
+});
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function toggleShuffle(option, event) {
+  if (option === "none") {
+    // Restore the original state
+    Object.keys(originalState).forEach((key) => {
+      document.querySelectorAll(".category-container")[key].innerHTML =
+        originalState[key];
+    });
+  } else if (option === "categories") {
+    document.querySelectorAll(".category-container").forEach((category) => {
+      let signs = Array.from(category.querySelectorAll(".sign-container"));
+      shuffleArray(signs);
+      signs.forEach((sign) => category.appendChild(sign));
+    });
+  } else if (option === "all") {
+    let allSigns = Array.from(document.querySelectorAll(".sign-container"));
+    shuffleArray(allSigns);
+    let gallery = document.querySelector("#gallery"); // Replace with your main container ID
+    gallery.innerHTML = ""; // Clear the container
+    allSigns.forEach((sign) => gallery.appendChild(sign));
+  }
+
+  // Update button appearance for shuffle buttons
+  const buttons = document.querySelectorAll(".shuffle-btn");
+  buttons.forEach((btn) => btn.classList.remove("selected"));
+  if (event) {
+    event.currentTarget.classList.add("selected");
   }
 }
