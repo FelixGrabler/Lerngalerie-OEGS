f1965a57b8f45d929a6795f43d4c1bdce11e51c3
Felix Grabler <grabler@student.tugraz.at>
2024-01-10 15:25:40 +0100

    added expire 1 year to http headers

--- src/app.js
+++ src/app.js
@@ -4,33 +4,30 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.get("/mp4/:filename", (req, res) => {
+  const filename = req.params.filename;
+  const options = {
+    root: path.join(__dirname, "..", "public", "mp4"),
+    headers: {
+      "Cache-Control": "public, max-age=31536000", // 1 year
+      Expires: new Date(Date.now() + 31536000000).toUTCString(), // 1 year from now
+    },
+  };
+
+  res.sendFile(filename, options, (err) => {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    }
+  });
+});
 
 app.get("/", (req, res) => {
   console.log("GET /");
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/videos", (req, res) => {
-  const mp4Directory = path.join(__dirname, "/public/mp4");
-  fs.readdir(mp4Directory, (err, files) => {
-    if (err) {
-      console.error("Fehler beim Lesen des Verzeichnisses:", err);
-      res
-        .status(500)
-        .send(
-          "Serverfehler beim Lesen des Verzeichnisses" +
-            mp4Directory +
-            "! " +
-            err
-        );
-      return;
-    }
-    console.log("MP4-Dateien loaded.");
-    const mp4Files = files.filter((file) => file.endsWith(".mp4"));
-    res.json(mp4Files);
-  });
-});
+app.use(express.static("public"));
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running on http://0.0.0.0:${port}`);
